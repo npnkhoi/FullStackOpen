3ab9107dc685c3136e162e1038b50efcb5b1926b
npnkhoi <npnkhoi@gmail.com>
2021-05-25 08:26:22 +0700

    refactor into folders

---
+++ blog-list/app.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const express = require('express')
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+app.use('/api/blogs', blogsRouter)
+
+logger.info(config)
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+  .then(() => {
+    logger.info('connected to MongoDB')
+  }).catch((e) => {
+    logger.error('error connecting to mongoDB:', e.message)
+  })
+
+module.exports = app
